703d4396551e9bbe40d65b2bd9011943826b3cce
IAmIlliest <k.i.hampel+github@gmail.com>
2017-12-31 17:26:32 +1100

    Revised version that meets the requirement to use a function and a function callback.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+# Assumes that all things returned by 'ls -l' are condsidered to be 'files', including directories and links etc.
+# Custom exit codes 3-5 (inclusive) used to try and indicate where an error originated from or bubbled up to.
+
 response="___init___"
 num_files=$(expr $(ls -l | wc -l) - 1)
 
+function checkGtOrLt () {
+	local result=""
+	if [[ $1 -eq $num_files ]]; then let result="0"
+	elif [[ $1 -gt $num_files ]]; then let result="1"
+	elif [[ $1 -lt $num_files ]]; then let result="2"
+	else let result="3"
+	fi
+	echo $result
+}
 
 while $([[ $response =~ [0-9]+ ]] || [[ $response -eq "___init___" ]])
 do
@@ -10,18 +22,22 @@ do
 	read response
 
 	if [[ $response =~ [0-9]+ ]]; then
-		if [[ $response -eq $num_files ]]; then
+		if [[ $(checkGtOrLt $response) -eq 0 ]]; then
 			echo "You guessed correctly, well done!"
 			exit 0
-		elif [[ $response -gt $num_files ]]; then
+		elif [[ $(checkGtOrLt $response) -eq 1 ]]; then
 			echo "Your guess is higher than the number of files. Please guess again."
-		elif [[ $response -lt $num_files ]]; then
+		elif [[ $(checkGtOrLt $response) -eq 2 ]]; then
 			echo "Your guess is lower than the number of files. Please guess again."
+		elif [[ $(checkGtOrLt $response) -eq 3 ]]; then
+			echo "You provided an unquantifiable response, please provide an integer next time you play."
+			exit 3
 		else
-			echo "Some unhandled exception."
-			exit 1
+			echo "Some other unhandled exception."
+			exit 4
 		fi
 	fi
 done
 
-exit 1
+# Catch all error code for any weird cases.
+exit 5
